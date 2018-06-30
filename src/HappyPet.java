aed02e9d52d796d28b220c620e77c9c8a6436e63
poap <League@imac-3.attlocal.net>
2018-06-29 18:26:47 -0700

    i did stuff like happy pet and finished pi aloud. Choose your own adventure is unfinished

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -1,29 +1,83 @@
 import javax.swing.JOptionPane;
 
 public class HappyPet {
-	
+	static String pet;
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+           pet = JOptionPane.showInputDialog(null, "What kind of pet do you want to buy?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
+		for(int i=0;i<100 ;i++) {
+			
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+          int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?", "Happy Pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "cuddle", "food", "water", "take a walk", "groom", "clean up poop" }, null);
 
+        
 			// 5. Use user input to call the appropriate method created in step 4.
-
+                if (task == 0) {
+                	cuddle();
+                }
+                else if (task == 1) {
+                	food();
+                }
+                else if (task == 2) {
+                	water();
+                }
+                else if (task == 3) {
+                	takeAWalk();
+                }
+                else if (task == 4) {
+                	groom();
+                }
+                else {
+                	cleanUpPoop();
+                }
+                
+                
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+                   if (happinessLevel==500) {
+                	   JOptionPane.showMessageDialog(null, "You love your pet");
+                	   break;
+                   }
 	}
-
-	// 4. Create methods to handle each of your user selections.
+	}
+	static // 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	void cuddle() {
+		JOptionPane.showMessageDialog(null, pet + " will feel loved.");
+	    happinessLevel = happinessLevel + 10;
+	    
+	    
+	    
+	    
+	}
+	static void food() {
+		JOptionPane.showMessageDialog(null, pet + " will be very happy.");
+	    happinessLevel = happinessLevel + 20;
+	}
+	static void water() {
+		JOptionPane.showMessageDialog(null, pet + " will be grateful to you for not letting it die from lack of water.");
+	    happinessLevel = happinessLevel + 30;
+	}
+	static void takeAWalk() {
+		JOptionPane.showMessageDialog(null, pet + " will thank you for the exersise that you don't get enough of.");
+	    happinessLevel = happinessLevel + 40;
+	}
+	static void groom() {
+		JOptionPane.showMessageDialog(null, pet + " will be happy about this.");
+	    happinessLevel = happinessLevel + 50;
+	}
+	static void cleanUpPoop() {
+		JOptionPane.showMessageDialog(null, pet + " will be eternally grateful for not making them eat the poop.");
+	    happinessLevel = happinessLevel + 100;
+	}
+	
+	
+
 }
--- src/PiAloud.java
+++ src/PiAloud.java
@@ -5,10 +5,12 @@
 import java.io.IOException;
 import java.util.Scanner;
 
+import org.omg.Messaging.SyncScopeHelper;
+
 public class PiAloud {
 	public static void main(String[] args) {
 		
-	}
+	
 
 	// 1. Make a main method and make sure your program can run
 
@@ -17,12 +19,31 @@ String pi = "3.14159265358979323846264338327950288419716939937510582097494459230
 
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 
+
+for (int i = 0; i<1000000; i++) {
+	 char a = getInputFromUser(); 
+	 if (a == pi.charAt(i)) {
+		 System.out.println("correct");
+	 }
+	 else {
+		System.err.println("incorrect");
+	 }
+	System.out.println(pi.charAt(i));
+     speak(pi.charAt(i));
+     
+}
+
+	
+
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
 
 	// 5. Use the speak() method to speak all the digits of Pi.
 
 	// [ADVANCED]
 	// *6. Get a character from the user using the getInputFromUser() method
+	
+		
+	}
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
 
@@ -41,7 +62,7 @@ String pi = "3.14159265358979323846264338327950288419716939937510582097494459230
 			e.printStackTrace();
 		}
 	}
-
+	
 
 	static char getInputFromUser() {
 		Scanner scanner = new Scanner(System.in);
@@ -49,5 +70,6 @@ String pi = "3.14159265358979323846264338327950288419716939937510582097494459230
 	}
 
 }
+
 
 
